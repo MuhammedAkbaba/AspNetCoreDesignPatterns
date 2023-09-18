957e592a47c989286f1f51a7ea8c0697e8aaf004
Muhammed Akbaba <bymdakbaba@gmail.com>
2023-09-18 12:42:55 +0300

    Template 3/2   Adım

---
+++ UdemyAspNetCoreDesignPatterns/WebApp.Template/UserCards/UserCardTemplate.cs
@@ -0,0 +1,44 @@
+﻿
+using BaseProject.Models;
+using System.Text;
+
+namespace WebApp.Template.UserCards
+{
+    public abstract class UserCardTemplate
+    {
+        protected AppUser AppUser { get; set; }
+
+        public UserCardTemplate(AppUser appUser)
+        {
+            AppUser = appUser;
+        }
+
+        /*İşlem sırasını belirlendiği metotdur. UML diyagramına göre :" Template method" olur.
+         Template Metodu ve algoritmadaki sıralı operasyonları fiziksel olarak barındıran soyut sınıftır.*/
+        public string Build()
+        {
+            if (AppUser == null) throw new ArgumentNullException(nameof(AppUser));
+
+            var sb = new StringBuilder();
+            sb.Append("<div class='card'>");
+            sb.Append(SetPicture());
+            sb.Append($@"<div class='card-body'>
+                          <h5>{AppUser.UserName}</h5>
+                          <p>{AppUser.Description}</p> ");
+            sb.Append(SetFooter());
+            sb.Append(" </div>");
+
+
+            sb.Append("</div>");
+
+            return sb.ToString();
+
+        }
+        protected abstract string SetFooter();
+        protected abstract string SetPicture();
+
+        
+
+
+    }
+}
