4ac3698ec26b5877ab88c83ca429d2fffc2b6026
Muhammed Akbaba <bymdakbaba@gmail.com>
2023-09-25 12:15:30 +0300

    Command Project

---
+++ UdemyAspNetCoreDesignPatterns/WebAp.Command/Models/AppIdentityDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BaseProject.Models
+{
+    public class AppIdentityDbContext:IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options):base(options)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);  
+        }
+    }
+}
