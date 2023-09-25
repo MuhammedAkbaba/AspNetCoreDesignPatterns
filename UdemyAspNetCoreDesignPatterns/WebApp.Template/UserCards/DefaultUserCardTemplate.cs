887b11697e10de42284cf7496d86415a8644032a
Muhammed Akbaba <bymdakbaba@gmail.com>
2023-09-25 10:24:10 +0300

    Template

---
+++ UdemyAspNetCoreDesignPatterns/WebApp.Template/UserCards/DefaultUserCardTemplate.cs
@@ -0,0 +1,26 @@
+﻿using BaseProject.Models;
+using System.Text;
+
+namespace WebApp.Template.UserCards
+{
+    /// <summary>
+    /// Üye olmayan kullanıcı template
+    /// </summary>
+    public class DefaultUserCardTemplate : UserCardTemplate
+    {
+
+        protected override string SetFooter()
+        {
+            return string.Empty;
+        }
+
+        protected override string SetPicture()
+        {
+            var sb = new StringBuilder();
+            sb.Append($@"<img src='/userpictures/defaultUser.png'  class='card-img-top'  height='300px' width='30px'");
+            return sb.ToString();
+
+        }
+
+    }
+}
---
+++ UdemyAspNetCoreDesignPatterns/WebApp.Template/UserCards/UserCardTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Net;
+
+namespace WebApp.Template.UserCards
+{
+    //<user-card app-user= /> şeklinde olur
+    public class UserCardTagHelper : TagHelper
+    {
+
+        public AppUser AppUser { get; set; }
+        private readonly IHttpContextAccessor httpContextAccessor;
+        public UserCardTagHelper(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            UserCardTemplate userCard;
+            if (httpContextAccessor.HttpContext.User.Identity.IsAuthenticated)
+                userCard = new PrimeUserCardTemplate();
+            else
+                userCard = new DefaultUserCardTemplate();
+
+
+            userCard.SetUser(AppUser);
+
+            output.Content.SetHtmlContent(userCard.Build());
+
+        }
+
+    }
+}
